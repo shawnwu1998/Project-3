6b92264d64c6e8db7da9de46f10a6b151c6085b1
yhan123 <44731952+yhan123@users.noreply.github.com>
2019-02-25 15:55:57 -0500

    Update Solver.java

--- Solver.java
+++ Solver.java
@@ -1,33 +1,38 @@
-
 public class Solver {
 
+	//check complete (all variables are assigned a domain)
 	public static boolean isComplete(CSP c) {
+		
 		for(int i=0; i<c.var.length; i++) {
-			if(c.var[i].assignemnt==null) {
+			if(c.var[i].assimt==null) {
 				return false;
 			}
 		}
-		return true;
+		return true;	
 		
 	}
 	
+	//check consistent 
 	public static boolean isConsistent(CSP c) {
 		
-		for(int i=0; i<c.cons1.size(); i++) {
+		for(int i=0; i<c.cons1.size(); i++) {//go through all pairs of consistent
 			String city1= c.cons1.get(i);
 			String city2= c.cons2.get(i);
 			
 			String ass1 = " ";
 			String ass2 = " ";
 			
+			//when variable equals name of the consistent
+			//get the domain
 			for(int j=0; j<c.var.length; j++ ) {
 				if(c.var[j].name ==city1) {
-					ass1 = c.var[j].assignemnt;
+					ass1 = c.var[j].assimt;
 				}if(c.var[j].name == city2) {
-					ass2 = c.var[j].assignemnt;
+					ass2 = c.var[j].assimt;
 				}
 			}
 			
+			//two adjacent cities have the same color
 			if(ass1!=null && ass1 == ass2) {
 				return false;
 			}
@@ -37,27 +42,31 @@ public class Solver {
 		
 	}
 	
-	public boolean Consistent(CSP c, int i, String value) {
-		c.var[i].assignemnt=value;
+	public boolean Consistent(CSP c, int i, String value) {//c, position, domain
+		c.var[i].assimt=value;//set assignment to this value (domain)
 		return isConsistent(c);
-				
+
 	}
 	
 	public CSP backtrack(CSP c) {
 		if(isComplete(c)) {
 			return c;
 		}
+		
+		//check if each var has a assignment
+		//if does not have assignment, set index to it
 		int index=0;
 		for(int i=0 ; i<c.var.length; i++) {
-			if(c.var[i].assignemnt==null) {
+			if(c.var[i].assimt==null) {
 				index = i;
 				break;
 			}
 		}
 		
+		//set value to the var's domain when the assignment is consistent
 		for(String values: c.var[index].dm) {
 			if(Consistent(c, index, values)) {
-				c.var[index].assignemnt=values;
+				c.var[index].assimt=values;
 				CSP result = backtrack(c);
 				if(result!=null) {
 					return result;
