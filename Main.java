2e3b324851dfef39462ed9ee7ae402058cfd4e55
xwang151 <44735068+xwang151@users.noreply.github.com>
2019-02-25 21:34:17 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,14 @@
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Australia Map");
 		AustraliaCSP map = new AustraliaCSP();
 		map.amaptraliaMap();
 
+		System.out.println("Job Shop");
+		JobShop job = new JobShop();
+		job.jobshop();
+		
 	}
 
 }
