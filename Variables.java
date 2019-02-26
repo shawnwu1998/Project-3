56695799f273fb03e38d5dba001702fc4a5bf8ef
xwang151 <44735068+xwang151@users.noreply.github.com>
2019-02-25 21:32:53 -0500

    Update Variables.java

--- Variables.java
+++ Variables.java
@@ -3,15 +3,24 @@ import java.util.HashSet;
 public class Variables {
 
 	public String name;
-	String assignemnt=null;
+	public String assign=null;
 	public HashSet<String> dm = null;
+	public int value=0;
+	public HashSet<Integer> dmj=null;
 	
 	public Variables(String name, String assignment, HashSet<String> domain) {
 		this.name=name;
-		this.assignemnt=assignment;
+		this.assign=assignment;
 		this.dm=domain;
+	}
+	
+	public Variables(String name, int value, HashSet<Integer> domain) {
+		this.name=name;
+		this.value=value;
+		this.dmj=domain;
+		
+		
 	}
 	
 	
 }
-
