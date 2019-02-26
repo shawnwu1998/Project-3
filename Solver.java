673a3e8fcf5580cb9aa01b85e848943dcd50c03f
xwang151 <44735068+xwang151@users.noreply.github.com>
2019-02-25 21:32:33 -0500

    Update Solver.java

--- Solver.java
+++ Solver.java
@@ -2,82 +2,205 @@ public class Solver {
 
 	//check complete (all variables are assigned a domain)
 	public static boolean isComplete(CSP c) {
-		
+
 		for(int i=0; i<c.var.length; i++) {
-			if(c.var[i].assimt==null) {
+			if(c.var[i].assign==null) {
 				return false;
 			}
 		}
 		return true;	
-		
+
 	}
-	
-	//check consistent 
+
+	public static boolean isComplete(CSP c, int value) {
+
+		for(int i=0; i<c.var.length; i++) {
+			if(c.var[i].value==0) {
+				return false;
+			}
+		}
+		return true;	
+
+	}
+	public boolean Consistent(CSP c, int i, String value) {//c, position, domain
+		c.var[i].assign=value;//set assignment to this value (domain)
+		return isConsistent(c);
+
+	}
+
+	public boolean Consistent(CSP c, int i, int v) {//c, position, domain
+		c.var[i].value=v;//set assignment to this value (domain)
+		boolean consistent1 = isConsistentJob(c);
+		boolean consistent2 = isConsistentJob2(c);
+		if(isConsistentJob(c)==false) {
+			return false;
+		}
+		if(isConsistentJob2(c)==false) {
+			return false;
+		}
+		else {
+			return true;
+		}
+
+	}
+	//check consistent for Map
 	public static boolean isConsistent(CSP c) {
-		
+
 		for(int i=0; i<c.cons1.size(); i++) {//go through all pairs of consistent
 			String city1= c.cons1.get(i);
 			String city2= c.cons2.get(i);
-			
+
 			String ass1 = " ";
 			String ass2 = " ";
-			
+
 			//when variable equals name of the consistent
 			//get the domain
 			for(int j=0; j<c.var.length; j++ ) {
 				if(c.var[j].name ==city1) {
-					ass1 = c.var[j].assimt;
+					ass1 = c.var[j].assign;
 				}if(c.var[j].name == city2) {
-					ass2 = c.var[j].assimt;
+					ass2 = c.var[j].assign;
 				}
 			}
-			
+
 			//two adjacent cities have the same color
 			if(ass1!=null && ass1 == ass2) {
 				return false;
 			}
-			
+
 		}
 		return true;
-		
-	}
-	
-	public boolean Consistent(CSP c, int i, String value) {//c, position, domain
-		c.var[i].assimt=value;//set assignment to this value (domain)
-		return isConsistent(c);
 
 	}
-	
+
+	//check first consistent for job
+	public boolean isConsistentJob(CSP c) {
+
+		for(int i=0; i<c.cons3.size(); i++) {
+			String leftside = c.cons3.get(i);
+			String rightside = c.cons4.get(i);
+
+			int inspection = 0;
+			int val1 = 0;
+			int val2 = 0;
+
+			for(int j=0; j<c.var.length; j++) {
+
+				if(c.var[j].name.equals("Inspection")) {
+					inspection= c.var[j].value;			
+				}
+				if(c.var[j].name.equals(leftside)) {
+					if(leftside.equals("AxleF") || leftside.equals("AxleB")) {
+						val1 = c.var[j].value + 10;
+					}if(leftside.equals("WheelRF") || leftside.equals("WheelLF")  || leftside.equals("WheelRB")  || leftside.equals("WheelLB") ) {
+						val1 = c.var[j].value + 1;
+					}if (leftside.equals("NutsRF") || leftside.equals("NutsLF")  || leftside.equals("NutsRB")  || leftside.equals("NutsLB") ) {
+						val1 = c.var[j].value + 2;	
+					}else {
+						val1 = c.var[j].value;
+					}
+
+				}
+				if(c.var[j].name==rightside) {
+					val2 = c.var[j].value;
+				}		
+
+			}
+
+			if((val1 > val2 && val1!=0 && val2 !=0) ||  (val1>inspection && val1!=0 && inspection!=0) ) {
+				return false;
+
+			}
+
+		}
+
+		return true;
+
+	}
+
+	//check second consistent for job
+	public boolean isConsistentJob2(CSP c) {
+		for(int i=0; i<c.cons3.size(); i++) {
+			String dis1 = c.cons3.get(i);
+			String dis2 = c.cons4.get(i);
+			int val1=0;
+			int val2=0;
+			for(int j= 0; j<c.var.length; j++) {
+				if (c.var[j].name.equals(dis1)) {
+					val1 = c.var[j].value + 10;
+				}if(c.var[j].name.equals(dis2)) {
+					val2 = c.var[j].value;
+				}		
+			}
+
+			if((val1 > val2 || val2+10>val2) && val1!=0 && val2!=0) {
+				return false;
+			}
+
+		}	
+
+		return true;
+
+
+	}
+
+
+
 	public CSP backtrack(CSP c) {
 		if(isComplete(c)) {
 			return c;
 		}
-		
+
 		//check if each var has a assignment
 		//if does not have assignment, set index to it
 		int index=0;
 		for(int i=0 ; i<c.var.length; i++) {
-			if(c.var[i].assimt==null) {
+			if(c.var[i].assign==null) {
 				index = i;
 				break;
 			}
 		}
-		
+
 		//set value to the var's domain when the assignment is consistent
 		for(String values: c.var[index].dm) {
 			if(Consistent(c, index, values)) {
-				c.var[index].assimt=values;
+				c.var[index].assign=values;
 				CSP result = backtrack(c);
 				if(result!=null) {
 					return result;
 				}
 			}
-			
-			
 		}
-		
+
 		return null;
-		
+
 	}
-	
+
+	public CSP backtrack(CSP c, int value) {
+		if(isComplete(c,0)) {
+			return c;
+		}
+		int index=0;
+		for(int i=0 ; i<c.var.length; i++) {
+			if(c.var[i].value == 0) {
+				index = i;
+				break;
+			}
+		}
+
+		for(Integer values: c.var[index].dmj) {
+			
+			if(Consistent(c, index, values)) {
+				
+				c.var[index].value=values;
+				CSP result = backtrack(c,0);
+				if(result!=null) {
+					return result;
+				}
+			}
+		}
+		return null;
+
+	}
+
 }
