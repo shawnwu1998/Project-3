99bb64d88a14d80aff8fc7004a4b3f8ad614b481
xwang151 <44735068+xwang151@users.noreply.github.com>
2019-03-03 11:48:24 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Australia Map");
-		AustraliaCSP map = new AustraliaCSP();
-		map.amaptraliaMap();
-
-		System.out.println("Job Shop");
-		JobShop job = new JobShop();
-		job.jobshop();
+		System.out.println("Please choose your game:");
 		
+		while(true) {
+			System.out.println("1. Australia Map    2.  Job-Shop Scheduling   3. N-Queens ");
+			System.out.println("Enter 0 to quit");
+			Scanner scan = new Scanner(System.in);
+			int x = scan.nextInt();
+			if(x==1) {
+				System.out.println("Australia Map");
+				AustraliaCSP map = new AustraliaCSP();
+				map.amaptraliaMap(); 	
+			}
+			if(x==2) {
+				System.out.println("Job Shop");
+				JobShop job = new JobShop();
+				job.jobshop();
+			}
+			if(x==0) {
+				break;
+			}
+		}
 	}
 
 }
